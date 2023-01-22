2c46dba304159aaf86a6f55dfb50c0d76e3dbf44
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-01-22 20:19:36 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+var textContainer = document.getElementById('evaluatedText')
+var display = document.getElementById('wordCount') 
+
+var words = textContainer.value.split(' ').length;
+// display.innerHTML = 'words'
+textContainer.addEventListener('input' , countWords);
+function countWords(){
+    display.innerHTML = 'words';
+}
