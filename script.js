907874264bb59e278372f174a5dec70ceb4f83fa
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-01-24 11:28:57 +0530

    done

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 var textContainer = document.getElementById('evaluatedText')
 var display = document.getElementById('wordCount') 
 
-// var words = textContainer.value.split(" ");
-// display.innerHTML = 'words'
+
 textContainer.addEventListener('input' , countWords);
 function countWords(){
-    display.innerHTML = textContainer.value.split(" ").length;
+    
+    display.innerHTML = textContainer.value.split(" ").length-1;
+    if(textContainer.value == ''){
+        display.innerHTML = '0';
+    }
 }
