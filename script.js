2a56cf4a3cc38b2fc8f62a5c27dbf626d25b06fe
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-01-24 11:34:54 +0530

    updated

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var display = document.getElementById('wordCount')
 
 textContainer.addEventListener('input' , countWords);
 function countWords(){
-    
-    display.innerHTML = textContainer.value.split(" ").length-1;
-    if(textContainer.value == ''){
-        display.innerHTML = '0';
-    }
+    let text = textContainer.value.trim();
+   // display.innerHTML = textContainer.value.split(" ").length;
+    ///if(textContainer.value == ''){
+       // display.innerHTML = '0';
+    //}
+    let count = text.length > 0?text.split(" ").length: 0;
+    display.innerHTML = count;
 }
